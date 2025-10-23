9e4df044c2f13731d21b9d06edf8adf9ef336ede
Cline, Caleb N CTR (USA) <caleb.n.cline.ctr@mail.mil>
2025-10-23 18:45:42 +0000

    changed to the govcloud ubuntu24.04 ami ID

--- labs/aws/diagnostics-cogitator/main.tf
+++ labs/aws/diagnostics-cogitator/main.tf
@@ -15,6 +15,7 @@ terraform {
       version = "~> 3.5"
     }
   }
+  backend "local" {}
 }
 
 provider "aws" {
@@ -44,19 +45,9 @@ data "aws_availability_zones" "available" {
   state = "available"
 }
 
-data "aws_ami" "ubuntu_22" {
-  most_recent = true
-  owners      = ["099720109477"]
-
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
-  }
-  
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
+# Ubuntu 24.04 AMI for GovCloud
+locals {
+  ubuntu_ami = "ami-0a300eb29e1e99010"
 }
 
 # VPC Module
