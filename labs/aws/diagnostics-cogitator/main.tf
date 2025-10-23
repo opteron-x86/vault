da3f85ab755161677fee8b1b82fa6eb695572209
Cline, Caleb N CTR (USA) <caleb.n.cline.ctr@mail.mil>
2025-10-23 18:47:37 +0000

    fix line 269

--- labs/aws/diagnostics-cogitator/main.tf
+++ labs/aws/diagnostics-cogitator/main.tf
@@ -266,7 +266,7 @@ resource "aws_ebs_volume" "target_volume" {
 
 # EC2 Instance
 resource "aws_instance" "cogitator_vm" {
-  ami                    = data.aws_ami.ubuntu_22.id
+  ami                    = local.ubuntu_ami
   instance_type          = local.instance_type
   subnet_id              = module.vpc.public_subnet_ids[0]
   vpc_security_group_ids = [
