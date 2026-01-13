62dd2262e3f8dce57cf85098bf4f2edf855965e2
Cline, Caleb N CTR (USA) <caleb.n.cline.ctr@mail.mil>
2026-01-13 18:36:43 +0000

    prepare 1.5.0 release

--- vault/__init__.py
+++ vault/__init__.py
@@ -3,6 +3,6 @@ VAULT - Virtual Attack Utility Lab Terminal
 A Python-based CLI tool for managing Terraform lab infrastructure.
 """
 
-__version__ = "1.4.7"
+__version__ = "1.5.0"
 __author__ = "Caleb N. Cline"
 __org__ = "DG35 - CTE"
